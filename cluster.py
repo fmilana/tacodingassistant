a73f17379fe29a83010f44238f1b4ddb89dac03c
fmilana <ucjufmi@ucl.ac.uk>
2021-01-26 19:05:46 +0100

    improve pre-processing regex

--- cluster.py
+++ cluster.py
@@ -11,21 +11,21 @@ from train import clean_text, clean_sentence
 
 model = Sentence2Vec('./data/word2vec.model')
 
-# text = open('text\\joint_groupbuy_jhim.txt', 'r').read()
-text = clean_text(open('text\\joint_reorder_exit.txt', 'r').read())
+# file_name = 'joint_groupbuy_jhim'
+file_name = 'joint_reorder_exit'
+text = clean_text(open('text\\' + file_name + '.txt', 'r').read())
 sentences = [clean_sentence(sentence) for sentence in sent_tokenize(text)]
 
-sentence_embeddings = np.array(
-    [model.get_vector(sentence) for sentence in sentences
-    if not re.match('[.,…:;–\'’!?-]', sentence)])
+sentence_embeddings = np.array([model.get_vector(sentence)
+                                for sentence in sentences
+                                if not re.match('[.,…:;–\'’!?-]', sentence)])
 
 kmeans = KMeans(n_clusters=8)
 kmeans.fit(sentence_embeddings)
 
-# !! Get the indices of the points for each corresponding cluster
+# get the indices of the points for each corresponding cluster
 mydict = {i: np.where(kmeans.labels_ == i) for i in range(kmeans.n_clusters)}
-
-# Transform the dictionary into list
+# transform the dictionary into list
 dictlist = []
 for cluster, labels in mydict.items():
     dictlist.append([cluster, labels])
@@ -48,54 +48,8 @@ def testsentence(num):
     print('sentence: "' + model.get_sentence(sentence_embeddings[num]) + '"')
     print('cluster: ' + str(get_cluster_from_label(num)))
     print('confidence: ' + str(confidence(sentence_embeddings[num],
-        kmeans.cluster_centers_[get_cluster_from_label(num)])))
+           kmeans.cluster_centers_[get_cluster_from_label(num)])))
 
 
 for i in range(15):
     testsentence(i)
-
-
-
-
-# # reduce dimensionality to 2
-# pca = PCA(n_components=2)
-# reduced_embeddings = pca.fit_transform(sentence_embeddings)
-#
-# kmeans_reduced = KMeans(n_clusters=8)
-# kmeans.fit(reduced_embeddings)
-#
-# # plot clustering #
-#
-# # Step size of the mesh. Decrease to increase the quality of the VQ.
-# h = .02     # point in the mesh [x_min, x_max]x[y_min, y_max].
-#
-# # Plot the decision boundary. For that, we will assign a color to each
-# x_min = reduced_embeddings[:, 0].min() - 1
-# x_max = reduced_embeddings[:, 0].max() + 1
-# y_min = reduced_embeddings[:, 1].min() - 1
-# y_max = reduced_embeddings[:, 1].max() + 1
-# xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-#
-# # Obtain labels for each point in mesh. Use last trained model.
-# Z = kmeans_reduced.predict(np.c_[xx.ravel(), yy.ravel()])
-#
-# # Put the result into a color plot
-# Z = Z.reshape(xx.shape)
-# plt.figure(1)
-# plt.clf()
-# plt.imshow(Z, interpolation="nearest",
-#            extent=(xx.min(), xx.max(), yy.min(), yy.max()),
-#            cmap=plt.cm.Paired, aspect="auto", origin="lower")
-#
-# plt.plot(reduced_embeddings[:, 0], reduced_embeddings[:, 1], 'k.', markersize=2)
-# # Plot the centroids as a white X
-# centroids = kmeans_reduced.cluster_centers_
-# plt.scatter(centroids[:, 0], centroids[:, 1], marker="x", s=169, linewidths=3,
-#             color="w", zorder=10)
-# plt.title("K-means clustering on PCA-reduced sentence embeddings\n"
-#           "Centroids are marked with white cross")
-# plt.xlim(x_min, x_max)
-# plt.ylim(y_min, y_max)
-# plt.xticks(())
-# plt.yticks(())
-# plt.show()
--- train.py
+++ train.py
@@ -10,38 +10,37 @@ from gensim.models import Word2Vec
 def clean_text(text):
     text = text.lower()
     # remove interviewer
-    text = re.sub('iv[0-9]*[ \t].*', '', text)
+    text = re.sub(r'iv[0-9]*[ \t].*', '', text)
     # remove interview format
-    regexp = ('p[0-9]+\w*|speaker key|r*user\s*\d+( - study \d+)*|'
-        '(iv[0-9]*|ie|um|a[0-9]+)\t|'
-        '(interviewer|interviewee|person [0-9]|participant)|'
-        '\d{2}:\d{2}:\d{2}|\[(.*?)\]|\[|\]')
+    regexp = (r'p[0-9]+\w*|speaker key|r*user\s*\d+( - study \d+)*|'
+              '(iv[0-9]*|ie|um|a[0-9]+)\t|'
+              '(interviewer|interviewee|person [0-9]|participant)|'
+              '\d{2}:\d{2}:\d{2}|\[(.*?)\]|\[|\]')
     text = re.sub(regexp, '', text)
     # replace multiple spaces and newlines with one space
-    text = re.sub('\s+', ' ', text)
+    text = re.sub(r'\s+', ' ', text)
     return text
 
 
 def clean_sentence(sentence):
-    return re.sub('[.,…:;–\'’!?-]', '', sentence)
+    return re.sub(r'[^A-Za-z ]+', '', sentence)
 
 
-# text = open('text\\joint_groupbuy_jhim.txt', 'r').read()
-text = clean_text(open('text\\joint_reorder_exit.txt', 'r').read())
+# file_name = 'joint_groupbuy_jhim'
+file_name = 'joint_reorder_exit'
+text = clean_text(open('text\\' + file_name + '.txt', 'r').read())
 # tokenize and clean sentences
 sentences = [clean_sentence(sentence) for sentence in sent_tokenize(text)]
 # tokenize sentences into words and remove "." or "..." sentences
 sentences_tokenized = [word_tokenize(sentence) for sentence in sentences]
 
-# refer to here for all parameters:
 # https://radimrehurek.com/gensim/models/word2vec.html
 model = Word2Vec(sentences_tokenized, sg=1, size=100, window=5,
                  min_count=1, workers=4, iter=100)
-
 # save model to file
 model.save('./data/word2vec.model')
-# for word2vec2tensor
-model.wv.save_word2vec_format('./data/word2vecformat.model')
+# for word2vec2tensor (tensorboard)
+model.wv.save_word2vec_format('./data/word2vectf.model')
 
 # for tensorboard:
-# python -m gensim.scripts.word2vec2tensor -i data/word2vecformat.model -o data/
+# python -m gensim.scripts.word2vec2tensor -i data/word2vectf.model -o data/
