cf3dcf73cb8152282eb725f5f471ba0089187c60
fmilana <ucjufmi@ucl.ac.uk>
2021-05-11 21:40:07 +0200

    update readme and push docx/csv's

--- README.md
+++ README.md
@@ -2,16 +2,24 @@
 
 Using XGBoost to classify word2vec embeddings and generate coding suggestions.
 
-1) Classify the .docx:
+1) Install Python packages:
+```
+pip install -r requirements.txt
+```
+2) Install Node.js packages:
+```
+npm i body-parser csvtojson doxtract express
+```
+3) Classify the .docx:
 ```
 python classify_docx.py text/reorder_exit.docx
 ```
-2) Generate the word frequency .csv:
+4) Generate the word frequency .csv:
 ```
 python analyse_predictions.py
 ```
-3) Run the local server:
+5) Run the local server:
 ```
 node app.js
 ```
-4) Visit http://localhost:3000/index.html for text classification and http://localhost:3000/words.html for word frequencies for each theme.
+6) Visit http://localhost:3000/index.html for text classification and http://localhost:3000/words.html for word frequencies for each theme.
--- app.js
+++ app.js
@@ -1,25 +1,11 @@
 var express = require('express');
-var multer = require('multer');
-const bodyParser = require('body-parser');
-// var mammoth = require('mammoth');
-const { extractText } = require('doxtract');
-var childProcess = require('child_process');
+var bodyParser = require('body-parser');
+var extractText = require('doxtract');
 var csvtojson = require('csvtojson');
 
 var app = express();
 
 app.use(bodyParser.json());
-
-var storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function(req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
-
-var upload = multer({ storage: storage });
 
 app.use('/static', express.static('static'));
 
@@ -33,13 +19,11 @@ app.get('/words.html', function(req, res) {
 
 app.get('/get_html', function(req, res) {
   extractText('text/reorder_exit.docx').then((text) => {
-    // without python script --------------------------------
     csvtojson().fromFile('text/reorder_exit_train.csv')
       .then((trainObj) => {
         csvtojson().fromFile('text/reorder_exit_predict.csv')
           .then((predictObj) => {
             var jsonObj = [];
-            // to-do: better way
             text = text.replace('/"/g', "'");
             text = text.replace('/’/g', "'");
             jsonObj.push({'wholeText': text});
@@ -98,47 +82,8 @@ app.get('/get_html', function(req, res) {
             res.json(jsonObj);
           });
       });
-
-
-    // with python script -------------------------------------
-    // console.log('running classify_docx.py...');
-    // var spawn = childProcess.spawn;
-    // var pythonProcess = spawn('python', ['classify_docx.py', 'text/reorder_exit.docx']);
-    //
-    // pythonProcess.on('exit', function() {
-    //   console.log('extracting from train csv...');
-    //   csvtojson().fromFile('text/reorder_exit_train.csv')
-    //     .then((trainObj) => {
-    //       console.log('extracting from predict csv...');
-    //       csvtojson().fromFile('text/reorder_exit_predict.csv')
-    //         .then((predictObj) => {
-    //           var jsonObj = [];
-    //           // to-do: better way
-    //           text = text.replace('/"/g', "'");
-    //           text = text.replace('/’/g', "'");
-    //           jsonObj.push({'wholeText': text});
-    //
-    //           for (i = 0; i < trainObj.length; i++) {
-    //             var trainSentence = trainObj[i]['original sentence'];
-    //             jsonObj.push({'trainSentence': trainSentence});
-    //           }
-    //           console.log('pushed all train sentences!');
-    //           for (i = 0; i < predictObj.length; i++) {
-    //             if (predictObj[i]['practices'] == 1 || predictObj[i]['social'] == 1 || predictObj[i]['study vs product'] == 1 || predictObj[i]['system perception'] == 1 || predictObj[i]['system use'] == 1 || predictObj[i]['value judgements'] == 1) {
-    //               var predictSentence = predictObj[i]['original sentence'];
-    //               // console.log('pushing predictSentence:', predictSentence);
-    //               jsonObj.push({'predictSentence': predictSentence});
-    //             }
-    //           }
-    //           console.log('pushed all predict sentences!');
-    //           console.log('returning object to client');
-    //           res.json(jsonObj);
-    //         });
-    //     });
-    // });
   });
 });
-
 
 app.get('/get_data', function(req, res) {
   csvtojson().fromFile('text/reorder_exit_analyse.csv')
