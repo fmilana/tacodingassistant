17f135607ecf9435cda03b797c2fc8978f2b0a2f
fmilana <ucjufmi@ucl.ac.uk>
2023-09-29 15:24:08 +0100

    lower confidence threshold and increase oversampling

--- augment.py
+++ augment.py
@@ -49,7 +49,8 @@ def nearest_neighbour(X: pd.DataFrame, neigh) -> list:
     return indices
 
 
-def MLSMOTE(X, y, n_sample, neigh=5):
+# default neigh=5
+def MLSMOTE(X, y, n_sample, neigh=15):
     """
     Give the augmented data using MLSMOTE algorithm
     
--- main.py
+++ main.py
@@ -71,7 +71,8 @@ def show_error_popup(traceback):
 def load_table_data(doc_path, themes, table_name, reclassified):
     table = []
 
-    minimum_proba = 0.95
+    # default = 0.95
+    minimum_proba = 0.60
 
     if table_name == 'all-table':
         if reclassified:
@@ -263,7 +264,8 @@ class TextThread(QThread):
 
     def run(self):
         print('=========================== TEXT THREAD STARTED ===========================')
-        minimum_proba = 0.95
+        # default = 0.95
+        minimum_proba = 0.60
 
         document = docx.Document(self.app_window.doc_path)
         whole_text = []
